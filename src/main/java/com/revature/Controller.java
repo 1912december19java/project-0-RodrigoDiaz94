2b862a3bdea5d2dc793da759b4f5cf8ced616607
Revature Loaner <rodrigo.a.diaz94@gmail.com>
2019-12-19 08:58:08 -0500

    Starting off authentication

---
+++ src/main/java/com/revature/Controller.java
@@ -0,0 +1,18 @@
+package com.revature;
+
+import java.io.Console;
+import java.util.Scanner;
+
+public class Controller {
+
+	public Controller() {
+		Console console = System.console();
+		
+	Scanner userAnswer = new Scanner(System.in);
+	System.out.println("Have you previously registered (Y/N)?");
+	
+		
+				
+	}
+	
+}
