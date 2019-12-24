2cd9dec2cf08a7900232f35df6d097d79ded0e9e
Revature Loaner <rodrigo.a.diaz94@gmail.com>
2019-12-23 21:38:12 -0500

    Revised input reading and evaluation after further study

--- src/main/java/com/revature/Controller.java
+++ src/main/java/com/revature/Controller.java
@@ -1,18 +1,26 @@
 package com.revature;
 
-import java.io.Console;
 import java.util.Scanner;
+import java.util.InputMismatchException;
 
 public class Controller {
 
-	public Controller() {
-		Console console = System.console();
-		
-	Scanner userAnswer = new Scanner(System.in);
-	System.out.println("Have you previously registered (Y/N)?");
-	
-		
-				
+	public static void main(String args[]) {
+
+		Scanner userInput = new Scanner(System.in);
+
+		System.out.print("Welcome! Are you already registered (Y/N?)");
+
+		try {
+
+			String answerOne = userInput.next();
+			if (!answerOne.equalsIgnoreCase("Y") || !answerOne.equalsIgnoreCase("N")) {
+				throw new InputMismatchException("Not a valid input");
+			}
+		} catch (InputMismatchException e) {
+			e.printStackTrace();
+		}
+
+		userInput.close();
 	}
-	
 }
